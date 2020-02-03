9c2d3f3af4ddcb8090763aa717d5216c695bbd77
marshallbrekka <marshallb@fair.com>
2020-02-02 17:03:18 -0800

    More efficient displaying of images using an object URL

--- lib/Desktop.jsx
+++ lib/Desktop.jsx
@@ -1,11 +1,34 @@
 import styles from "./styles.jsx";
 const firefox = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAACXBIWXMAABYlAAAWJQFJUiTwAAAISklEQVRYCc1Xe4xVRx3+Zs6559zX7t67cGGX3aW8WxSqoaSV0hYUTKklFDQ0YDWa+IcSaUJobGhiDdGk0tQYqRpa0xATS0wkrcGUFkiRYhQs8gdNq9JKluUVtgt793Ef557XjN/c3bsuhZUSNXGS386cc2Z+3/d7zl3gvzy0hrgVlfJWNt9srx5es1UNrvnpzfb9T77HxdU/1MFarStrwvDK6uUfF+SW3DWR0vjKF74l09YuxNxhUaUfv4dKtEx0HSxOdKbx/j8OQe3Mg3OEjp8hKBBrAw64YoFyxc+1Xm81gCaaJySgf9OZ0vtmTp3oYOO9LYMtQqo8/AhQCmAWohpDOmKDujT4g8a+ieYbEijtbitAxb/zvXht46A+ck9n6fX7C41nMw+fXD5Z+uGXUAqAgARCWh9RDJEaSSTEU3H35zaPP/PR9XUEBrbncplytBdWtDLywqQ5oF+878v6iv3HsIr28QpS9sVFAtU2eB4BfRIgCUMgIgETDs7SlT8Kz6xY5XU/cr++uG7S+PNmfQ0BHhHNIvMToaxlKCo4YTAv+O6yx1GWL+uiaC9d0OWGgn1Lz0+7cHjN54fenwvvUo4EaiTAz3VPjCPCjLCk3FUpRQviONypT96VaOgw8zVV4D8x64tONn6FyDxGSSoPVjoJOym0TquBodx9B3/5WqqlZWirDfkZ+qZVQQvhlpHpPI1ZK9/AtHvfoVqTexRJEbTRTaDar/fJlDXPcdQL1m0HnjfgZowR0I/PcZUljsl0vAgJQ4BWJDmnOKepJGOjXJ7efXTP7i7pzUnYsgxZTzp6W2l4TAOvFmDGZ/dj8TdeJmejnsZaNlEs6Fh6vsJZN61ahkViYe62/QNmx1gIfC/5gIjsRajxVUDmPsXj4TKlxOAMx8g2nZu1dP0TiUTHeyiLJtQsF5HN/bYFNxNi7kPHEOoM/rZvBZTHMwHFH5mFFilXYK5wREcqClYZcDPGCEjfWi1iMg4IaAgYxTmuJ6XogTQTis8DCrmmk1i29jF8YvkOyEIvqo6DmmPDRwrFnllY+OhBzH3kBHTo0gCCG2F6GBEQ9fiLCA/V0RsEtpOIDK3FNImMCWozMrPp02mcp/K5MwPMZAK35UmmGYmch9tXvYC7Nu5AjeGpOgn4roW+izPxh+c2wVIRrBYishhQpQ4WSV0YUVCtpeWnTr44kox1D3x15comEdhd8PlorB8g+x4GMTDlRDGlZVEmSfj5LvS796CnuBFH3noaJZGBRw94DEPEfOk934X3D91NcKImTGIQtEISRiLmkwlLJNs65k9h6dBW80dUnRwztaUeEH6vj+4sUKSCNh66I0R5qIAjb34NH1xawW7LdqCaYCsbrhsjDAUschfECK0IPafmYeHK17jH9AYaEjojXi0YQg7DI9O+JwmAK3UC5YttlnJrUlpEbxAwLIr0BB1QbXfw0m+34B/9S+DkE7BSAaxECY62WfayXjC2jiAppIPBYgq6pFiBVGaSsESv5ulBQUIVFyK0KDHpjnrg7Pm2yuxCn2cnw2ydgOY3QyTBQ57E+TPT8e7ZDtj5PkTMcqFSjLPLIkvQo5KZz6o1cY98xFFAT5ToVXoupvXDLMEqATuHmIgjZHQs/P4PLZOaIyF4E22DSwd7etO5agGGWIOA4ppK2vL9SLZcxpBfgHBISoZMJBcOCcT0QMzkioOYIfcRsRcUJp+l1dTuJ6D6kxAFdkhJvBKLgKKUuPqXC2rQEKBvgBN4I/4mli7NN3l30owR6+skGFhamIxqKLEkT/XOppt9KB0zN0OoiMgENW1Y8z5Q1RocNYyH730VTcIApqEd3geTh8lQ0hN0f7kJfk0cXbJ13x6DXa8Csyh67iGvj2UW8pUpxfHSn8Fa9TaWTH2Hh8mpVAGGhhGXBhGUKZVBeNUyPN/H8vkH0C56gT72Dx1Cpghepk5WQfwh35VsDA2IQwbTjLoHzOJud2Fvm5/a2JQKmqVtvNAIhZklbAIvTn5gihgXawV4kY2IwQ/NzRcqFOQA1k07gAfaj9NaGpBkqGxTwgQ31tfo/iutCGqi78+X1Xf2nPg7mZnmNG6cym7+/ox0+HSm8xJscx9YVGCPKhpbh+jTrTgddeKqauYvMIUOq4jbk+fQ5DDREgRk30KSqh2ukxT2YL/YCpfn+uL4uak7f/1kA7Zeho2H45noZ5lKYkN8uTC3uf0qTGmhnogkYggYa3i5TLFLmGK9SwC+M6UmjPBbxHqPuA4IbggYMWnSS6KlPCqO7jnaH+1s4JmZO64dv89venhOaL2SyJbc3JRhuOZqFjH10xOGhOkVZjbA48WQMM9mmLUxjSH3a1nEbGJW0glPi2jDp/f/6tX6ntE/1xEw7481f3vLdC1+rJ2KyE4qI5tl2Ul6g+4WJDMC/BESdS/w8CgRTc9VBloQV1vhpB2cS0RPzX97945R3LHphgTM1z/lNm2dHtvPCOm7Kl1BU3OANHu9zTvBeEOQzIjrR+fRUCgl4bHbVYstsKvNELYVXshE37uz+4VnqXbURWP414fgX5+A13Ob1t4RJ57NCj3PE6xzx4fLHymm/yfYJSVDIajT/BCOmB8hG09QTUEGaaR1kr1Id5+x/G3Li7v2jtc7fj2hBxqbnmx9rPPRuHXzZCW+wpB2aIYgYGYpekAbd3NIbTEtbN4NDhuLhapGb1GqPQfsyvPbBn5xvqHrRvNNCTQObWv7+owHveyqgpIreEUtsLWYQuvJyfgANfa7PvbEv/ZBHX7LKh3YPrS7u3H2380fm8CYEv73+8mu9fl1fmZSPrR4pSYwkNDlw26x//iFvQMjfMZ2//8v/gnw86VqXV9twAAAAABJRU5ErkJggg==";
 
+function convertDataURIToBinary(base64) {
+  var raw = window.atob(base64);
+  var rawLength = raw.length;
+  var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+  for(var i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return array;
+}
+
+function base64ToImageObject(base64) {
+  let bytes = convertDataURIToBinary(base64);
+
+  // Make a Blob from the bytes
+  var blob = new Blob([bytes], {type: 'image/png'});
+
+  return blob;
+}
+
+const firefoxBlob = base64ToImageObject(firefox);
+const blobUrl = URL.createObjectURL(firefoxBlob);
+
 const containerStyle = {
-  display: "grid",
-  gridAutoFlow: "column",
-  gridGap: "0px"
-};
+                        display: "grid",
+                        gridAutoFlow: "column",
+                        gridGap: "0px"
+                        };
 
 const desktopStyle = {
   lineHeight: "17px",
@@ -64,7 +87,7 @@ const renderSpace = ({index, focused, visible, windows, label}) => {
   }
   if (index == 6) {
     return (
-      <div key={label} style={contentStyle}><img style={imageStyle} src={`data:image/jpeg;base64,${firefox}`} /></div>
+      <div key={label} style={contentStyle}><img style={imageStyle} src={blobUrl} /></div>
     );
   }
   return (
